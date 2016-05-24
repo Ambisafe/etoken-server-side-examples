d5cc039b3b600b9192ebb767460e50d281be7f0a
Oleksii Matiiasevych <lastperson@gmail.com>
2016-05-24 20:39:49 +0700

    Update to example handler.

--- callback-handler.php
+++ callback-handler.php
@@ -1,5 +1,8 @@
 <?php
 $tx = $_POST['event'];
 echo "transaction hash: {$tx['transactionHash']}";
-echo "transaction comment / user account id for ICAP: {$tx['reference']}";
+echo "transaction comment: {$tx['reference']}";
+echo "ICAP: {$tx['icap']}";
 echo "amount: {$tx['value']}";
+echo "from: {$tx['from']}";
+echo "to: {$tx['to']}";
