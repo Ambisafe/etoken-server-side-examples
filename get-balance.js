3887ca54c5e8bd24e1cca1ca792f88e6ce49d422
Oleksii Matiiasevych <lastperson@gmail.com>
2016-05-24 12:12:21 +0700

    get-balance.js works

--- get-balance.js
+++ get-balance.js
@@ -1,1 +1,6 @@
-console.log(100500);
+var Web3 = require('web3');
+var web3 = new Web3();
+var config = require('./config.js');
+var eToken = web3.eth.contract(config.abi).at(config.address);
+web3.setProvider(new web3.providers.HttpProvider(config.gethNode));
+console.log(eToken.balanceOf(process.argv[2], config.symbol).valueOf());
