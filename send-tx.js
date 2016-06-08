8277ae0011dd00f170e900bbcaa024aae43b7223
Oleksii Matiiasevych <lastperson@gmail.com>
2016-06-08 15:09:38 +0530

    Fix typos

--- send-tx.js
+++ send-tx.js
@@ -12,7 +12,7 @@ var asset = EToken.web3.eth.contract(config.abi).at(config.address);
 var destination = process.argv[2];
 var amount = EToken.web3.toBigNumber(process.argv[3]).mul(Math.pow(10, config.baseUnit));
 if (amount.decimalPlaces() !== 0) {
-  throw "Provide " + amount.decimalPlaces() + " less fractional digits in the amount: " + amount.valueOf() + " . Only " + config.baseUnit + " fractional digits allowed.";
+  throw "Provide " + amount.decimalPlaces() + " less fractional digits in the amount: " + process.argv[3] + " . Only " + config.baseUnit + " fractional digits allowed.";
 }
 var handler = function(err, tx) {
   if (err) {
@@ -28,7 +28,7 @@ if (EToken.web3.isAddress(destination)) {
 } else {
   if (destination.length !== 13
     && !(destination.length === 16 && destination.slice(0, 3) === config.icapAssetCode)
-    && !(destination.length === 20 && destination.slice(4, 3) === config.icapAssetCode))
+    && !(destination.length === 20 && destination.slice(4, 7) === config.icapAssetCode))
   {
     throw "Invalid ICAP format. Used: " + destination + ". It should be 13, 16, or 20 chars long: AMBICLIENT123, " + config.icapAssetCode + "AMBICLIENT123, " + EToken.web3.eth.iban.fromBban(config.icapAssetCode + "AMBICLIENT123").toString() + ".";
     process.exit(1);
