9b7b1a891b3e4bd9ba4ceaf8dd78eed4f9200b94
Oleksii Matiiasevych <lastperson@gmail.com>
2016-08-31 15:44:54 +0300

    Update callback-handler.php

--- callback-handler.php
+++ callback-handler.php
@@ -1,8 +1,8 @@
 <?php
-$tx = $_POST['event'];
+$tx = json_decode(file_get_contents('php://input'), TRUE);
 echo "transaction hash: {$tx['transactionHash']}";
-echo "transaction comment: {$tx['reference']}";
-echo "ICAP: {$tx['icap']}";
-echo "amount: {$tx['value']}";
-echo "from: {$tx['from']}";
-echo "to: {$tx['to']}";
+echo "transaction comment: {$tx['eventData']['reference']}";
+echo "currency: {$tx['eventData']['symbol']}";
+echo "amount: {$tx['eventData']['value']}";
+echo "from: {$tx['eventData']['from']}";
+echo "to: {$tx['eventData']['to']}";
