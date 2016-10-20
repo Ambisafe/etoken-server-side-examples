fbf0425a83ed70f2f76e23108d1871d997e77069
Oleksii Matiiasevych <lastperson@gmail.com>
2016-10-20 19:29:58 +0300

    Update invocation-example.php

--- invocation-example.php
+++ invocation-example.php
@@ -7,11 +7,11 @@ echo "balance: $balance";
 #send transaction to regular address
 $destination = '0x1ff21eca1c3ba96ed53783ab9c92ffbf77862584';
 $amount = 1.12;
-$txid = `node send-tx.js $destination`;
+$txid = `node send-tx.js $destination $amount`;
 echo "transaction hash: $txid";
 
 #send transaction to icap address
 $destination = 'AMBICLIENT123';
 $amount = 1.12;
-$txid = `node send-tx.js $destination`;
+$txid = `node send-tx.js $destination $amount`;
 echo "second transaction hash: $txid";
